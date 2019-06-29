9f7baf3686010582576528fb1f827067e2b5a540
j3N0 <3223529700@qq.com>
2019-06-29 17:21:09 +0800

    Customized Swagger with metadata.

--- src/main/java/com/example/spring5mvcrest/config/SwaggerConfig.java
+++ src/main/java/com/example/spring5mvcrest/config/SwaggerConfig.java
@@ -3,9 +3,13 @@ package com.example.spring5mvcrest.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @EnableSwagger2
 @Configuration
@@ -17,6 +21,23 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+
+        Contact contact = new Contact("J3N0", "https://github.com/j3N0", "3223529700@qq.com");
+
+        return new ApiInfo(
+                "Restful SpringBoot App",
+                "Some demo",
+                "1.0",
+                "",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>()
+        );
     }
 }
